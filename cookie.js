88a4784878b265987e2cb40742c6c438f63f2f4d
Kyu <kyudeci@gmail.com>
2018-12-02 16:37:37 -0500

    Separate js file for cookie.html

---
+++ cookie.js
@@ -0,0 +1,18 @@
+let suggest = document.getElementById("suggest");//input
+let submit2 = document.getElementById("submit2");//sumit button
+
+function order() {
+  let para = document.createElement("p");//paragraph element
+  let orderBox = document.getElementById("display");// container for new paragraph tag
+  let code = document.createTextNode(suggest.value);// input value
+  orderBox.appendChild(para);
+  para.appendChild(code);
+  para.classList.add("typewriter");
+  suggest.value = "";
+}
+submit2.addEventListener("click", order);
+suggest.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    document.getElementById("submit2").click();
+  }
+});
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-//Index.html portion
 let codeA = document.getElementById("secret");
 let submit = document.getElementById("submit");
 let imgUnlock = document.getElementById("unlock");
@@ -6,52 +5,34 @@ let titleUnlock = document.getElementById("unlock-title");
 let textUnlock = document.getElementById("unlock-text");
 let btnUnlock = document.getElementById("unlock-btn");
 let btnUnlock2 = document.getElementById("unlock-btn2");
-let invalid = document.getElementById("invalid")
+let invalid = document.getElementById("invalid");
 
-// function search() {
-//   if (codeA.value == "cheesecake") {
-//     alert("You've unlocked something!");
-//     imgUnlock.src = "Padlock-open.png";
-//     titleUnlock.innerHTML = "It Appears to Have Opened!";
-//     textUnlock.innerHTML = "You figured out the secret code! Don't share it or do share it if you want to! Click to reveal the new option...";
-//     // *Note: I don't have cookies on this site so this will lock on refresh!
-//     codeA.value =""
-//     btnUnlock2.style.display = "inline-block";
-//     invalid.innerHTML = ""
-//   } else {
-//     invalid.innerHTML = "Invalid Code!"
-//   }
-// }
-// function unknown() {
-//   imgUnlock.src = "http://assets.kraftfoods.com/recipe_images/opendeploy/52544_640x428.jpg";
-//   titleUnlock.innerHTML = "The One, The Only, Cheesecake!";
-//   textUnlock.innerHTML = "Wonderful, Beautiful, Magnificent, Cheesecake-y goodness.";
-//   btnUnlock.style.display = "inline-block";
-//   btnUnlock2.parentNode.removeChild(btnUnlock2);
-// }
-// submit.addEventListener("click", search);
-// btnUnlock2.addEventListener("click", unknown);
-// codeA.addEventListener("keyup", function(event) {
-//   if (event.keyCode === 13) {
-//     document.getElementById("submit").click();
-//   }
-// });
-//Cookie.html portion
-let suggest = document.getElementById("suggest");//input
-let submit2 = document.getElementById("submit2");//sumit button
-
-function order() {
-  let para = document.createElement("p");//paragraph element
-  let orderBox = document.getElementById("display");// container for new paragraph tag
-  let code = document.createTextNode(suggest.value);// input value
-  orderBox.appendChild(para);
-  para.appendChild(code);
-  para.classList.add("typewriter");
-  suggest.value = "";
+function search() {
+  if (codeA.value == "cheesecake") {
+    alert("You've unlocked something!");
+    imgUnlock.src = "Padlock-open.png";
+    titleUnlock.innerHTML = "It Appears to Have Opened!";
+    textUnlock.innerHTML = "You figured out the secret code! Don't share it or do share it if you want to! Click to reveal the new option...";
+    // *Note: I don't have cookies on this site so this will lock on refresh!
+    codeA.value = "";
+    btnUnlock2.style.display = "inline-block";
+    invalid.innerHTML = "";
+  } else {
+    invalid.innerHTML = "Invalid Code!";
+    codeA.value = "";
+  }
 }
-submit2.addEventListener("click", order);
-suggest.addEventListener("keyup", function(event) {
+function unknown() {
+  imgUnlock.src = "http://assets.kraftfoods.com/recipe_images/opendeploy/52544_640x428.jpg";
+  titleUnlock.innerHTML = "The One, The Only, Cheesecake!";
+  textUnlock.innerHTML = "Wonderful, Beautiful, Magnificent, Cheesecake-y goodness.";
+  btnUnlock.style.display = "inline-block";
+  btnUnlock2.parentNode.removeChild(btnUnlock2);
+}
+submit.addEventListener("click", search);
+btnUnlock2.addEventListener("click", unknown);
+codeA.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
-    document.getElementById("submit2").click();
+    document.getElementById("submit").click();
   }
 });
